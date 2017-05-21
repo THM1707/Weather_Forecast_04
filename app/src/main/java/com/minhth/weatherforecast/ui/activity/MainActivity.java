7e58002bc9b7283bdc244bfc47268b011114358c
THM1707 <tranhoangminh1707@gmail.com>
2017-05-21 22:15:07 +0700

    06_ui_part_1

--- app/src/main/java/com/minhth/weatherforecast/ui/activity/MainActivity.java
+++ app/src/main/java/com/minhth/weatherforecast/ui/activity/MainActivity.java
@@ -5,12 +5,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -24,7 +32,22 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.minhth.weatherforecast.R;
+import com.minhth.weatherforecast.data.model.ForecastResponseModel;
+import com.minhth.weatherforecast.data.model.WeatherModel;
 import com.minhth.weatherforecast.service.GpsService;
+import com.minhth.weatherforecast.service.WeatherService;
+import com.minhth.weatherforecast.ui.adapter.DailyAdapter;
+import com.minhth.weatherforecast.ui.adapter.HourlyAdapter;
+import com.minhth.weatherforecast.util.ConditionUtils;
+import com.minhth.weatherforecast.util.TimeUtils;
+import com.minhth.weatherforecast.util.WeatherServiceGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener {
@@ -32,15 +55,36 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private static final String EXCLUDE = "flags";
     private static final int REQUEST_CHECK_SETTINGS = 1;
     private static final int REQUEST_FINE_LOCATION = 101;
+    private static final int FIRST_DAY = 0;
+    private static final int LAST_DAY = 7;
+    private static final int LAST_HOUR = 23;
+    private static final int FIRST_HOUR = 0;
     private GoogleApiClient mGoogleApiClient;
     private GpsService mGpsService;
+    private Toolbar mToolbar;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private TextView mTextTime, mTextTemperature, mTextCondition, mTextFeel, mTextDegree;
+    private TextView mTextPromptHourly, mTextPromptDaily;
+    private RecyclerView mRecyclerHourly, mRecyclerDaily;
+    private HourlyAdapter mHourlyAdapter;
+    private DailyAdapter mDailyAdapter;
+    private ImageView mImageMainCondition;
+    private List<WeatherModel> mHourlyData = new ArrayList<>();
+    private List<WeatherModel> mDailyData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         buildGoogleApiClient();
         requestGps();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
     }
 
     @Override
@@ -88,6 +132,42 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mGoogleApiClient.disconnect();
     }
 
+    private void initViews() {
+        LinearLayoutManager layoutManager
+            = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerDaily = (RecyclerView) findViewById(R.id.recycler_daily);
+        mRecyclerDaily.setLayoutManager(new LinearLayoutManager(this) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        });
+        mTextPromptDaily = (TextView) findViewById(R.id.text_prompt_daily);
+        mTextPromptHourly = (TextView) findViewById(R.id.text_prompt_hourly);
+        mDailyAdapter = new DailyAdapter(mDailyData);
+        mRecyclerDaily.setAdapter(mDailyAdapter);
+        mRecyclerHourly = (RecyclerView) findViewById(R.id.recycler_hourly);
+        mRecyclerHourly.setLayoutManager(layoutManager);
+        mHourlyAdapter = new HourlyAdapter(mHourlyData);
+        mRecyclerHourly.setAdapter(mHourlyAdapter);
+        mTextDegree = (TextView) findViewById(R.id.text_degree);
+        mTextTime = (TextView) findViewById(R.id.text_time);
+        mTextFeel = (TextView) findViewById(R.id.text_real_feel);
+        mTextTemperature = (TextView) findViewById(R.id.text_temperature);
+        mTextCondition = (TextView) findViewById(R.id.text_condition);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_main);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                getLocation();
+            }
+        });
+        mImageMainCondition = (ImageView) findViewById(R.id.image_condition);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(mToolbar);
+    }
+
     private synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
             .addConnectionCallbacks(this)
@@ -98,10 +178,80 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     private void getLocation() {
         if (mGpsService.isCanGetLocation()) {
-            // TODO: 5/14/2017
+            WeatherService service = WeatherServiceGenerator.createService(WeatherService.class);
+            service.forecastResponse(WeatherServiceGenerator.API_KEY, mGpsService.getLatitude(),
+                mGpsService.getLongitude(), UNIT, EXCLUDE).enqueue(
+                new Callback<ForecastResponseModel>() {
+                    @Override
+                    public void onResponse(Call<ForecastResponseModel> call,
+                                           Response<ForecastResponseModel> response) {
+                        if (response != null) {
+                            ForecastResponseModel model = response.body();
+                            showData(model);
+                        }
+                        if (mSwipeRefreshLayout.isRefreshing()) {
+                            mSwipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ForecastResponseModel> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, R.string.error_something_happen,
+                            Toast.LENGTH_SHORT).show();
+                        if (mSwipeRefreshLayout.isRefreshing()) {
+                            mSwipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                });
         } else {
-            // TODO: 5/14/2017
+            Toast.makeText(MainActivity.this, R.string.error_no_service, Toast.LENGTH_SHORT)
+                .show();
         }
+    }
+
+    private void showData(ForecastResponseModel model) {
+        mTextPromptHourly.setText(R.string.title_hourly_forecast);
+        mTextPromptDaily.setText(R.string.title_daily_forecast);
+        if (model.getCurrently() != null) {
+            showCurrentlyData(model.getCurrently());
+        }
+        if (model.getHourly() != null) {
+            showHourlyData(model.getHourly());
+
+        }
+        if (model.getDaily() != null) {
+            showDailyData(model.getDaily());
+        }
+    }
+
+    private void showDailyData(ForecastResponseModel.WeatherBlock data) {
+        mDailyData.clear();
+        mDailyData.addAll(data.getWeatherModels().subList(FIRST_DAY, LAST_DAY));
+        mDailyAdapter.notifyDataSetChanged();
+    }
+
+    private void showHourlyData(ForecastResponseModel.WeatherBlock data) {
+        mHourlyData.clear();
+        List<WeatherModel> hourly = data.getWeatherModels().subList(FIRST_HOUR, LAST_HOUR);
+        mHourlyData.addAll(hourly);
+        mHourlyAdapter.notifyDataSetChanged();
+    }
+
+    private void showCurrentlyData(WeatherModel data) {
+        String celsius = getResources().getString(R.string.symbol_celsius);
+        String temperature = String.valueOf((int) data.getTemperature());
+        String updateTime =
+            getResources().getString(R.string.title_last_update) + TimeUtils.nowToString();
+        String realFeel =
+            getResources().getString(R.string.title_feel) + (int) data.getApparentTemperature() +
+                celsius;
+        mTextTemperature.setText(temperature);
+        mTextTime.setText(updateTime);
+        mTextFeel.setText(realFeel);
+        mTextCondition.setText(ConditionUtils.getCondition(data.getIcon()));
+        mImageMainCondition
+            .setImageResource(ConditionUtils.getConditionResource(data.getIcon()));
+        mTextDegree.setText(celsius);
     }
 
     private void requestPermission() {
